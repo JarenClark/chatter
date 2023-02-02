b8843c01a2d28909f770b9a37fadb3c87076f1d3
JarenClark <jarenclark1@gmail.com>
2023-02-01 21:48:25 -0800

    initial setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+//import UserContext from '../lib/Store'
+//import { supabase, fetchUserRoles } from 'lib/Store'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
